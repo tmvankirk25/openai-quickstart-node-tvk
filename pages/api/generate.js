980f61a8d06f73cfd1c74ac0c3c613507ccf9427
tmvankirk25 <tmvk@umich.edu>
2023-05-23 13:15:02 -0500

    update for haiku generation

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -19,19 +19,21 @@ export default async function (req, res) {
   if (animal.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
+        message: "Please enter a topic",
       }
     });
     return;
   }
 
   try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(animal),
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{role:"user", content: generatePrompt(animal)}],
       temperature: 0.6,
     });
-    res.status(200).json({ result: completion.data.choices[0].text });
+    console.log(completion.data.choices[0].message.content)
+    res.status(200).json({ result: completion.data.choices[0].message.content });
+    console.log(completion.data.choices[0].message.content)
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
@@ -51,12 +53,9 @@ export default async function (req, res) {
 function generatePrompt(animal) {
   const capitalizedAnimal =
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+  return `Write a haiku about this word consting of three lines with 17 syllables total. The first lines is 5 syllables, the second line is 7 syllables, and the third line is 5 syllables. Separate each line with a period.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+
+word: ${capitalizedAnimal}
+haiku:`;
 }
